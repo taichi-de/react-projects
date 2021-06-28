d4796a5aa4e3b1518698aea663f9b758a4b9dd80
taichi-de <61309248+taichi-de@users.noreply.github.com>
2021-06-28 22:01:43 +0200

    react10(1/2)

--- 10-grocery-bud/setup/src/List.js
+++ 10-grocery-bud/setup/src/List.js
@@ -1,7 +1,39 @@
-import React from 'react'
-import { FaEdit, FaTrash } from 'react-icons/fa'
+import React from "react";
+import { FaEdit, FaTrash } from "react-icons/fa";
 const List = () => {
-  return <h2>list component</h2>
-}
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+  const handle = (e) => {
+    e.preventDefault();
+  };
 
-export default List
+  return (
+    <section className="section-center">
+      <form onSubmit={handleSubmit} className="grocery-form">
+        {alert.show && <Alert />}
+        <h3>grocery bud</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="e.g. eggs"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? "edit" : "submit"}
+          </button>
+        </div>
+        <div className="grocery-container">
+          <List />
+          <button className="clear-btn">clear items</button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default List;
