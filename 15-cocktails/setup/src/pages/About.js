800d62b66160fc1422bf6b40eef778d430bcc41f
taichi-de <61309248+taichi-de@users.noreply.github.com>
2021-07-06 11:53:29 +0200

    react15(1/3)

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+        animi, sed assumenda voluptate impedit quia voluptates aliquid quas
+        quasi magnam officiis maxime? Laborum cupiditate qui, dicta numquam nisi
+        dolorem aliquam?
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
