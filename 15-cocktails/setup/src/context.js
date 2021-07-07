6b09cd3c735a52b8b66011d87041cad027ab3774
taichi-de <61309248+taichi-de@users.noreply.github.com>
2021-07-07 17:46:29 +0200

    react15

--- 15-cocktails/setup/src/context.js
+++ 15-cocktails/setup/src/context.js
@@ -8,6 +8,37 @@ const AppProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
   const [searchTerm, setSearchTerm] = useState("a");
   const [cocktails, setCocktails] = useState([]);
+
+  const fetchDrinks = useCallback(async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`${url}${searchTerm}`);
+      const data = await response.json();
+      const { drinks } = data;
+      if (drinks) {
+        const newCocktails = drinks.map((item) => {
+          const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } =
+            items;
+          return {
+            id: idDrink,
+            name: strDrink,
+            image: strDrinkThumb,
+            info: strAlcoholic,
+            glass: strGlass,
+          };
+        });
+        setCocktails(newCocktails);
+      } else {
+        setCocktails([]);
+      }
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+    }
+  }, [searchTerm]);
+  useEffect(() => {
+    fetchDrinks();
+  }, [searchTerm, fetchDrinks]);
   return (
     <AppContext.Provider
       value={{ loading, searchTerm, cocktails, setSearchTerm }}
--- 21-movie-db/setup/src/SearchForm.js
+++ 21-movie-db/setup/src/SearchForm.js
@@ -1,7 +1,32 @@
-import React from 'react'
-import { useGlobalContext } from './context'
+import React from "react";
+import { useGlobalContext } from "./context";
 const SearchForm = () => {
-  return <h2>search component</h2>
-}
+  const { setSearchTerm } = useGlobalContext();
+  const searchValue = React.useRef("");
+  React.useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <section className="section search">
+      <form className="search-form">
+        <div className="form-control">
+          <label htmlFor="name">search your favorite cocktails</label>
+          <input
+            type="text"
+            id="name"
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+    </section>
+  );
+};
 
-export default SearchForm
+export default SearchForm;
